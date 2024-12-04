0a32b75adca55af77d78408f175b321871f8456e
Andy Killorin <37423245+Speedy6451@users.noreply.github.com>
2024-12-04 16:29:06 -0500

    return idle protobuf

--- Server/src/main.rs
+++ Server/src/main.rs
@@ -1,5 +1,32 @@
 mod protos;
+use anyhow::Result;
+use protobuf::{EnumOrUnknown, Message, MessageField};
+use protos::message::server_command::{State, StateChange};
+use protos::message::ServerCommand;
+use tokio::net::TcpListener;
+use axum::routing::get;
+use axum::Router;
 
-fn main() {
-    println!("Hello, world!");
+#[tokio::main]
+async fn main() -> Result<()> {
+    let port = 8080;
+    let listener = TcpListener::bind(format!("127.0.0.1:{port}")).await?;
+
+    let app = Router::new()
+        .route("/protobuf", get(data));
+
+    axum::serve(listener, app).await?;
+
+    Ok(())
+}
+
+async fn data() -> Vec<u8> {
+    println!("received request");
+    let mut message  = ServerCommand::new();
+    let mut state = StateChange::new();
+    state.state = EnumOrUnknown::new(State::IDLE);
+
+    message.stateChange = MessageField::some(state);
+
+    message.write_to_bytes().unwrap()
 }
