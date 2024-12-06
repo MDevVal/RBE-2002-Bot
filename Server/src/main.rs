834352cffa3184f3e1a61d2ee108f7b8fa52c1b6
Andy Killorin <37423245+Speedy6451@users.noreply.github.com>
2024-12-06 16:46:00 -0500

    logging

--- Server/src/main.rs
+++ Server/src/main.rs
@@ -6,9 +6,19 @@ use protos::message::ServerCommand;
 use tokio::net::TcpListener;
 use axum::routing::get;
 use axum::Router;
+use tracing::info;
+use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
 
 #[tokio::main]
 async fn main() -> Result<()> {
+    // logging
+    let stdout = tracing_subscriber::fmt::layer()
+        .compact()
+        .with_file(true)
+        .with_target(true);
+    let reg = tracing_subscriber::registry().with(stdout);
+    reg.try_init()?;
+
     let port = 8080;
     let listener = TcpListener::bind(format!("0.0.0.0:{port}")).await?;
 
@@ -22,10 +32,12 @@ async fn main() -> Result<()> {
 }
 
 async fn data() -> Vec<u8> {
-    println!("received request");
+    info!("received request");
     let mut message  = ServerCommand::new();
     let mut state = StateChange::new();
-    state.state = EnumOrUnknown::new(State::IDLE);
+
+    state.baseSpeed = 10.;
+    state.state = EnumOrUnknown::new(State::LINING);
 
     message.stateChange = MessageField::some(state);
 
