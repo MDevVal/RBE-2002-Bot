f1860dbddcd65fe2bea2ebe2713df499df3c61c2
Benni Valentine <bvalentine@wpi.edu>
2024-12-10 13:51:34 -0500

    Did andy's todo

--- ESP/src/main.cpp
+++ ESP/src/main.cpp
@@ -2,15 +2,16 @@
 #include <WiFi.h>
 #include <esp_wifi.h>
 
-#include <pb_decode.h>
-#include <pb_encode.h>
 #include "message.pb.h"
 #include <Interface.h>
 #include <ServerInterface.h>
+#include <pb_decode.h>
+#include <pb_encode.h>
 
-const char* ssid = "RBE";
-const char* password = "elm69wisest16poisoned";
-const char* serverURL = "http://130.215.137.221:8080/nestState/:id:";  // The server endpoint
+const char *ssid = "RBE";
+const char *password = "elm69wisest16poisoned";
+const char *serverURL =
+    "http://130.215.137.221:8080/nestState/:id:"; // The server endpoint
 
 Interface romiInterface = Interface(Serial1);
 ServerInterface server = ServerInterface(serverURL);
@@ -18,19 +19,19 @@ ServerInterface server = ServerInterface(serverURL);
 bool stateChange = false;
 WiFiClient client;
 
-uint8_t macHash(){
+uint8_t macHash() {
   uint8_t baseMac[6];
   esp_err_t ret = esp_wifi_get_mac(WIFI_IF_STA, baseMac);
   if (ret == ESP_OK) {
-      return baseMac[3] ^ baseMac[4] ^ baseMac[5];
+    return baseMac[3] ^ baseMac[4] ^ baseMac[5];
   } else {
-      return 0;
+    return 0;
   }
 }
 
 void setup() {
   Serial.begin(115200);
-  
+
   // Connect to Wi-Fi
   WiFi.begin(ssid, password);
   while (WiFi.status() != WL_CONNECTED) {
@@ -39,24 +40,27 @@ void setup() {
   }
   Serial.println("Connected to WiFi");
 
-  Serial.println(macHash()); // TODO: append to url
+  char fullURL[128];
+  snprintf(fullURL, sizeof(fullURL), "%s%d", serverURL, macHash());
 }
 
 void loop() {
   size_t msg_size;
-  if (!romiInterface.readUART(msg_size)) return;
+  if (!romiInterface.readUART(msg_size))
+    return;
 
   message_RomiData data = message_RomiData_init_default;
   if (msg_size == message_RomiData_size) {
 
-      // Decode the message from the Romi
-      if (!romiInterface.readProtobuf(data, message_RomiData_fields)) return; 
+    // Decode the message from the Romi
+    if (!romiInterface.readProtobuf(data, message_RomiData_fields))
+      return;
 
-      message_ServerCommand serverMessage = message_ServerCommand_init_default;
+    message_ServerCommand serverMessage = message_ServerCommand_init_default;
 
-      // Send the Romi data to the server, and send the response back to the Romi
-      if (server.HTTPRequest(data, serverMessage)) 
-        romiInterface.sendProtobuf(serverMessage, message_ServerCommand_fields, message_ServerCommand_size);
+    // Send the Romi data to the server, and send the response back to the Romi
+    if (server.HTTPRequest(data, serverMessage))
+      romiInterface.sendProtobuf(serverMessage, message_ServerCommand_fields,
+                                 message_ServerCommand_size);
   }
 }
-
